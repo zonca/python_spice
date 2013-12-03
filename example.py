09fa1aa8abf318f2088e900c8007593e3744fc3f
Andrea Zonca <code@andreazonca.com>
2013-12-03 15:10:02 -0800

    example

---
+++ example.py
@@ -0,0 +1,17 @@
+from __init__ import spice
+import healpy as hp
+import numpy as np
+
+lmax = 512
+input_cl = np.ones(lmax + 1)
+np.random.seed(1000)
+input_map = hp.synfast(input_cl, nside=256, pixwin=True)
+
+mask = np.ones_like(input_map)
+hp.write_map("total_mask.fits", mask)
+
+cl = spice(bin=False, norm=False, mapfile=input_map, nlmax=lmax, polarization="NO", beam_file="NO", beam_file2="NO")
+
+import matplotlib.pyplot as plt
+plt.plot(cl)
+plt.plot(input_cl)
