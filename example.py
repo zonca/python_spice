e0a31cd76de0e8b0af6385b7041dd16edaa644ce
Andrea Zonca <code@andreazonca.com>
2013-12-03 17:36:41 -0800

    improved plot

--- example.py
+++ example.py
@@ -13,5 +13,9 @@ hp.write_map("total_mask.fits", mask)
 cl = spice(bin=False, norm=False, mapfile=input_map, nlmax=lmax, polarization="NO", beam_file="NO", beam_file2="NO")
 
 import matplotlib.pyplot as plt
+plt.figure(figsize=(10,7))
 plt.plot(cl)
-plt.plot(input_cl)
+plt.plot(input_cl, color="red", linewidth=3)
+plt.xlim([0, lmax+1])
+plt.xlabel("$\ell$")
+plt.ylabel("$C_\ell$")
